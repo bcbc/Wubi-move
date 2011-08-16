cbf75ddf162cef38a71088e0d09b80bddb09196e
bcbc <openbcbc@gmail.com>
2011-08-15 23:55:46 -0700

    Add check for irregular partition mount points  These are not excluded by the migration script, and can result  in space problems (unintentionally copying other partitions to  the target partition).

--- wubi-move.sh
+++ wubi-move.sh
@@ -567,6 +567,38 @@ pre_checks ()
       fi
     fi
 
+# check for partitions mounted on 'unexpected' mountpoints. These aren't
+# included in the space check and causes the migration to run out of space
+# during the rsync copy. Mountpoints under /mnt or /media and of course
+# /boot, /usr, /home and /host are not a problem.
+    mtpt=
+    while read DEV MTPT FSTYPE OPTS REST; do
+        case "$DEV" in
+          /dev/sd[a-z][0-9])
+            mtpt=$MTPT
+            work=$MTPT
+            while true; do
+                work=${mtpt%/*}
+                if [ "$work" == "" ]; then
+                    break
+                fi
+                mtpt=$work
+            done
+            case $mtpt in
+            /mnt|/media|/host|/home|/usr|/boot)
+                true #ok
+                ;;
+            *)
+                echo "$0: "$DEV" is mounted on "$MTPT""
+                echo "$0: The migration script does not automatically"
+                echo "$0: exclude this mountpoint."
+                echo "$0: Please unmount "$MTPT" and try again."
+                exit_script 1
+                ;;
+            esac
+          ;;
+        esac
+    done < /proc/mounts
 
 # Ubuntu releases prior to 9.10 don't support ext4 (default)
     if ! type mkfs.ext4 > /dev/null 2>&1 ; then
